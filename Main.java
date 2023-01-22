5a840edc259364add1614963228ceeffe1403fb7
999Ruslan999 <113248592+999Ruslan999@users.noreply.github.com>
2023-01-22 12:54:23 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> inList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        System.out.println(inList);
+
+        Stream<Integer> stream = inList.stream();
+        inList.stream().
+                filter(x -> x > 0)
+                        .
+                filter(x -> x % 2 == 0)
+                        .
+                sorted(Comparator.naturalOrder());
+
+
+    }
+
+
+
+
+}
---
+++ Stream DZ/src/StreamMain.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMain {
+
+    List<Integer> inList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+
+
+}
